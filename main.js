1e58e32a85625d65ab661ed3708a28b10754fabe
NKVharshvardhan <73694587+NKVharshvardhan@users.noreply.github.com>
2022-08-02 19:34:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var nosex=0
+var nosey=0
+var rightwristx=0
+var leftwristx=0
+var difference=0
+
 function setup(){
     canvas=createCanvas(550,550);
     canvas.position(560,150);
@@ -13,11 +19,25 @@ function modelloaded(){
 
 function draw(){
 background("#0000FF");
+document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+fill("#00e6ff");
+stroke("#00e6ff");
+square(nosex,nosey,difference)
+
 }
 
 function gotposes(results){
 if(results.length>0)
 {
 console.log(results);
+nosex=results[0].pose.nose.x;
+nosey=results[0].pose.nose.y;
+console.log("nosex= "+nosex+"nosey= "+nosey);
+
+leftwristx=results[0].pose.leftwrist.x;
+rightwristx=results[0].pose.leftwrist.x;
+difference=floor(leftwristx-rightwristx); //floor is used to remove the decimal points
+console.log("leftwristx= "+leftwristx+"rightwristx= "+rightwristx);
+
 }
 }
